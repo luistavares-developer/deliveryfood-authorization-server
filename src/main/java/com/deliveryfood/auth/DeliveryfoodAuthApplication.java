641bb9488b81bc08a5b4ff9d7a9b4d89ad54e280
Felipe Tavares <luistavares.developer@gmail.com>
2020-11-01 15:43:01 -0300

    API de Authorization Server

---
+++ src/main/java/com/deliveryfood/auth/DeliveryfoodAuthApplication.java
@@ -0,0 +1,13 @@
+package com.deliveryfood.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryfoodAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryfoodAuthApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/deliveryfood/auth/config/AuthorizationServerConfig.java
@@ -0,0 +1,98 @@
+package com.deliveryfood.auth.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+import com.deliveryfood.auth.PkceAuthorizationCodeTokenGranter;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private RedisConnectionFactory redisConnectionFactory;
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+			.withClient("deliveryfood-web")
+				.secret(passwordEncoder.encode("root"))
+				.authorizedGrantTypes("password", "refresh_token")
+				.scopes("write", "read")
+				.accessTokenValiditySeconds(60 * 24 * 60 * 60)
+				.refreshTokenValiditySeconds(60 * 24 * 60 * 60)
+		.and()
+			.withClient("deliveryfood-backend")
+				.secret(passwordEncoder.encode("back"))
+				.authorizedGrantTypes("client_credentials")
+				.scopes("read")
+		.and()
+			.withClient("deliveryfood-analytics")
+				.secret(passwordEncoder.encode("analytics"))
+				.authorizedGrantTypes("authorization_code")
+				.scopes("read")
+				.redirectUris("http://aplicacao-cliente")
+		.and()
+			.withClient("web")
+				.authorizedGrantTypes("implicit")
+				.scopes("read")
+				.redirectUris("http://aplicacao-cliente")
+		.and()
+			.withClient("checktoken")
+				.secret(passwordEncoder.encode("check123"));
+		
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager)
+			     .userDetailsService(userDetailsService)
+			     .reuseRefreshTokens(false)
+				 .tokenStore(redisTokenStore())
+				 .tokenGranter(tokenGranter(endpoints));
+	}
+	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.checkTokenAccess("permitAll()");
+	}
+	
+	private TokenStore redisTokenStore() {
+		return new RedisTokenStore(redisConnectionFactory);
+	}
+	
+	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
+		var pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
+				endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
+				endpoints.getOAuth2RequestFactory());
+		
+		var granters = Arrays.asList(
+				pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
+		
+		return new CompositeTokenGranter(granters);
+	}
+}
